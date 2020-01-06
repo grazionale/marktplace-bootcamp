6b63bda2b5d1d98c84c3ad74612a25a534115bfd
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-06 15:28:34 -0300

    schema de usuário

---
+++ src/app/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true // Todos e-mails são salvos em caixa baixa
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', UserScheme)
