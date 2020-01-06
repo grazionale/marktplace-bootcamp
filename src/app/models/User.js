c1c893bd270fc7567797ce80c6b50590f25ff070
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-06 17:33:20 -0300

    Configurando JWT - Endpoint para gerar token

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../config/auth')
 
 const UserScheme = new mongoose.Schema({
   name: {
@@ -20,5 +23,29 @@ const UserScheme = new mongoose.Schema({
     default: Date.now
   }
 })
+
+/* Hook para acontecer antes de uma ação, neste caso, a ação save, que é executada quando uma informação está sendo
+atualizada ou salva */
+UserScheme.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 8) // encriptando a senha com bcrypt
+})
+
+UserScheme.methods = { // Objeto com todos os método que o User possuí
+  compareHash (password) { // Valida se a senha sem fornecida por parâmetro, é equivalente a senha criptografada do bd
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+UserScheme.statics = { // Métodos estáticos só podem ser chamados dentro do próprio User, e não através de uma instância de usuário como (new User())
+  generateToken ({ id }) { // Recebe um user e através da desistruturação está pegando apenas o id
+    return jwt.sign(
+      { id }, // Este objeto recebe quantas e quaisquer informações que você queira para utilizar dentro da criptografia do token
+      authConfig.secret, // Secret - informação única, que torna este token exclusivo desta aplicação
+      { expiresIn: authConfig.ttl }) // Tempo para o token expirar em milissegundos
+  }
+}
 
 module.exports = mongoose.model('User', UserScheme)
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const express = require('express')
 const routes = express.Router()
 
 const userController = require('./app/controllers/UserController')
+const sessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', userController.store) // Criar usuárioy
+routes.post('/sessions', sessionController.store) // Criar sessão
 
 module.exports = routes
