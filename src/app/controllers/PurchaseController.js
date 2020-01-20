7f102b8736b940bb01eb1b64bd4b7bc3abc6faec
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-20 08:26:43 -0300

    adicionando template no e-mail

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,12 @@ class PurchaseController {
       from: '"Gabriel Grazionale" <_1ntegrac0es##2019@gmail.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Conteúdo: ${content}</p>`
+      template: 'purchase', // Nome do arquivo localizado em views/emails/purchase.hbs, este caminho foi definido como configuração no Mail.js
+      context: {
+        user,
+        content,
+        ad: purchaseAd // ad: purchaseAd esta apenas renomeando purchaseAd para Ad, quando o template for ler na view.
+      }
     })
 
     return res.send()
--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,21 @@
 const nodemailer = require('nodemailer')
 const mailConfig = require('../../config/mail')
 
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars')
+const exphbs = require('express-handlebars')
+
+const viewPath = path.resolve(__dirname, '..', 'views', 'emails')
+
 const transport = nodemailer.createTransport(mailConfig)
+
+transport.use('compile', hbs({ // Configurar como nodemailer lida com os templates de e-mail
+  viewEngine: exphbs.create({
+    partialsDir: path.resolve(viewPath, 'partials'), // Templates parciais
+    defaultLayout: null
+  }),
+  viewPath,
+  extName: '.hbs' // Extensao utilizada par arquivos de template em hbs
+}))
 
 module.exports = transport
