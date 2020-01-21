bc9bdd740bde66638598aaffab0bc26c8a4bc3c7
Gabriel Grazionale <gabriel.fernandes@sotran.com.br>
2020-01-21 15:50:11 -0300

    inserindo o Sentry na fila para captura de erros

--- src/app/services/Queue.js
+++ src/app/services/Queue.js
@@ -1,4 +1,5 @@
 const kue = require('kue')
+const Sentry = require('@sentry/node')
 const redisConfig = require('../../config/redis')
 const jobs = require('../jobs')
 
@@ -8,5 +9,7 @@ const Queue = kue.createQueue({ redis: redisConfig })
  * Toda vez que o Node identificar que existe uma ação na fila que chama essa key, então ele dispara o handle.
  */
 Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle)
+
+Queue.on('error', Sentry.captureException) // Quando ocorrer um erro dentro da fila, o Sentry tb vai capturar
 
 module.exports = Queue
